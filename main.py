5805866187740e7bd371654885abadd15103e8b0
Patrick Hennig <patrick_hennig@fastmail.com>
2024-04-05 16:51:59 +0200

    small fix

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from ete3 import NCBITaxa
-
 ncbi = NCBITaxa()
 
 class TreeNode:
-    def init(self, name, taxid):
+    def __init__(self, name, taxid):
         self.name = name
         self.taxid = taxid
         self.children = {}
-    
+
     def add_child(self, child):
         if child.taxid not in self.children:
             self.children[child.taxid] = child
     
-    def repr(self):
+    def __repr__(self):
         return f"{self.name} ({self.taxid})"
 
 def insert_lineage(root, lineage, names):
@@ -28,25 +27,21 @@ def print_tree(node, depth=0):
     for child in node.children.values():
         print_tree(child, depth + 1)
 
-species_names = ['Dendroctonus ponderosae', 'Anoplophora glabripennis', 'Leptinotarsa decemlineata', 
-                 'Onthophagus taurus', 'Agrilus planipennis', 'Nicrophorus vespilloides', 
-                 'Sitophilus oryzae', 'Diabrotica virgifera', 'Asbolus verrucosus', 'Photinus pyralis',
-                 'Ignelater luminosus', 'Rhynchophorus ferrugineus', 'Tribolium madens', 
-                 'Dendroctonus ponderosae', 'Zophobas morio', 'Tenebrio molitor', 
-                 'Cylas formicarius', 'Acanthoscelides obtectus', 'Callosobruchus analis', 
-                 'Callosobruchus maculatus', 'Callosobruchus chinensis']
+species_names = [
+    'Dendroctonus ponderosae', 'Anoplophora glabripennis', 'Leptinotarsa decemlineata', 
+    'Onthophagus taurus', 'Agrilus planipennis', 'Nicrophorus vespilloides', 
+    'Sitophilus oryzae', 'Diabrotica virgifera', 'Asbolus verrucosus', 'Photinus pyralis', 
+    'Ignelater luminosus', 'Rhynchophorus ferrugineus', 'Tribolium madens', 
+    'Zophobas morio', 'Tenebrio molitor', 'Cylas formicarius', 
+    'Acanthoscelides obtectus', 'Callosobruchus analis', 'Callosobruchus maculatus', 'Callosobruchus chinensis'
+]
 
-# Assuming 'lineages' is a dict with species names as keys and their lineage (list of taxids) as values
-# 'names' is a dict with taxid as keys and taxonomic names as values
+# Prepare the data structures
 lineages = {}
 names = {}
-
 root = TreeNode("root", 1)
 
-for species, lineage in lineages.items():
-    insert_lineage(root, lineage, names)
-
-# Query NCBI and fill lineages and names
+# Query NCBI and fill `lineages` and `names`
 for species in species_names:
     taxid = ncbi.get_name_translator([species]).get(species, [None])[0]
     if taxid:
